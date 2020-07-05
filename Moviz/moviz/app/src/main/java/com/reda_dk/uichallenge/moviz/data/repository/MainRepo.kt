bcb9bd0a36d1923c651502eb955724ada3f555f9
Reda Dokkar <hr_dokkar@esi.dz>
2020-07-05 22:43:17 +0100

    added main repository

--- Moviz/moviz/app/src/main/java/com/reda_dk/uichallenge/moviz/data/repository/MainRepo.kt
+++ Moviz/moviz/app/src/main/java/com/reda_dk/uichallenge/moviz/data/repository/MainRepo.kt
@@ -1,11 +1,43 @@
 package com.reda_dk.uichallenge.moviz.data.repository
 
+import androidx.lifecycle.MutableLiveData
+import com.reda_dk.uichallenge.moviz.data.api.TmbdEndPoints
+import com.reda_dk.uichallenge.moviz.data.model.genres.Genres
+import com.reda_dk.uichallenge.moviz.data.model.movies.Movies
+import io.reactivex.disposables.CompositeDisposable
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class MainRepo {
 
-    val baseUrl = "https://api.themoviedb.org/3/"
-    val movizBaseUrl = "http://192.168.1.3:3000/"
-    val api_key = "2e27645e1938878aee2b80d8a00e81a1"
-    val imgBaseUrl = "https://image.tmdb.org/t/p/w500"
+    private val baseUrl = "https://api.themoviedb.org/3/"
+    private val movizBaseUrl = "http://192.168.1.3:3000/"
+    private val api_key = "2e27645e1938878aee2b80d8a00e81a1"
+    private val imgBaseUrl = "https://image.tmdb.org/t/p/w500"
 
 
+    private val moviesLiveData :MutableLiveData<Movies> = MutableLiveData()
+
+    private val genresLiveData :MutableLiveData<Genres> = MutableLiveData()
+
+
+    private val client = OkHttpClient
+                            .Builder()
+                            .build()
+
+    private val retrofit = Retrofit.Builder()
+                                .baseUrl(baseUrl)
+                                .addConverterFactory(GsonConverterFactory.create())
+                                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                                .client(client)
+                                .build()
+
+    private val apiservices = retrofit.create(TmbdEndPoints::class.java)
+
+
+    private val compositeDisposable = CompositeDisposable()
+
+    
 }
