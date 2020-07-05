66e377686a2e54bb5e422149c4f043f070a3299f
Reda Dokkar <hr_dokkar@esi.dz>
2020-07-05 23:18:21 +0100

    main view model done

--- Moviz/moviz/app/src/main/java/com/reda_dk/uichallenge/moviz/data/viewModel/MainVm.kt
+++ Moviz/moviz/app/src/main/java/com/reda_dk/uichallenge/moviz/data/viewModel/MainVm.kt
@@ -1,16 +1,27 @@
 package com.reda_dk.uichallenge.moviz.data.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.reda_dk.uichallenge.moviz.data.model.genres.Genres
+import com.reda_dk.uichallenge.moviz.data.model.movies.Movies
 import com.reda_dk.uichallenge.moviz.data.repository.MainRepo
 
 class MainVm : ViewModel() {
 
     private val mainRepo = MainRepo()
 
-    private val movieslv = mainRepo.getMoviesLv()
 
-    private val genreslv = mainRepo.getGenres()
+    fun getGenres(){
+        mainRepo.getGenres()
+    }
 
 
-    
+    fun getMovies(){
+        mainRepo.getMovies()
+    }
+
+
+    fun getGenresLiveData():LiveData<Genres>{return mainRepo.getGenresLv()}
+
+    fun getMoviesLiveData():LiveData<Movies>{return mainRepo.getMoviesLv()}
 }
