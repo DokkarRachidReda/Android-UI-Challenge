6fc97327990b920051705c4b8cfb62b205a80add
Reda Dokkar <hr_dokkar@esi.dz>
2020-07-05 22:52:12 +0100

    main repo done

--- Moviz/moviz/app/src/main/java/com/reda_dk/uichallenge/moviz/data/repository/MainRepo.kt
+++ Moviz/moviz/app/src/main/java/com/reda_dk/uichallenge/moviz/data/repository/MainRepo.kt
@@ -1,10 +1,15 @@
 package com.reda_dk.uichallenge.moviz.data.repository
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import com.reda_dk.uichallenge.moviz.data.api.TmbdEndPoints
 import com.reda_dk.uichallenge.moviz.data.model.genres.Genres
 import com.reda_dk.uichallenge.moviz.data.model.movies.Movies
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -12,13 +17,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainRepo {
 
+    //should be fetched at startup and passed with constructor
     private val baseUrl = "https://api.themoviedb.org/3/"
-    private val movizBaseUrl = "http://192.168.1.3:3000/"
     private val api_key = "2e27645e1938878aee2b80d8a00e81a1"
-    private val imgBaseUrl = "https://image.tmdb.org/t/p/w500"
 
 
     private val moviesLiveData :MutableLiveData<Movies> = MutableLiveData()
+        get() = field
 
     private val genresLiveData :MutableLiveData<Genres> = MutableLiveData()
 
@@ -39,5 +44,55 @@ class MainRepo {
 
     private val compositeDisposable = CompositeDisposable()
 
-    
+
+
+    fun getGenres(){
+
+        compositeDisposable.add(
+            apiservices.getGenres(api_key)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                . subscribe(this::onGenresResponse, this::onGenresFailure)
+        )
+    }
+
+    private fun onGenresFailure(t: Throwable) {
+
+        Log.e("Tmdb-api-genres","api call failed  : "+t.toString())
+    }
+
+    private fun onGenresResponse(response: Genres){
+
+        Log.e("Tmdb-api-genres","api call OK")
+
+        genresLiveData.value = response
+    }
+
+
+
+    fun getMovies(){
+
+        compositeDisposable.add(
+            apiservices.getLatestMovies(api_key)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                . subscribe(this::onMoviesResponse, this::onMoviesFailure)
+        )
+    }
+
+
+    private fun onMoviesFailure(t: Throwable) {
+
+        Log.e("Tmdb-api-movies","api call failed  : "+t.toString())
+
+    }
+
+
+    private fun onMoviesResponse(response: Movies){
+
+        Log.e("Tmdb-api-movies","api call OK")
+
+        moviesLiveData.value = response
+
+    }
 }
